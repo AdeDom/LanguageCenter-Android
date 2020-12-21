33ca8cbaec162530cb0e98ca87be209f423cdebd
AdeDom <pathiphonyw@gmail.com>
2020-12-21 08:54:24 +0700

    create nav for feature chat

---
+++ app/src/main/java/com/lc/android/presentation/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.lc.android.presentation.chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lc.android.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
